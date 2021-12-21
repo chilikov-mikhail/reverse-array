03de33cf7b85012a83a31ab42e929d5c8f03ffa2
Mikhail Chilikov <chilikov_mihail@mail.ru>
2021-12-21 09:01:14 +0400

    Update ReverseArray.java

--- src/main/java/ReverseArray.java
+++ src/main/java/ReverseArray.java
@@ -1,6 +1,5 @@
 public class ReverseArray {
 
-    //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
         for(int i = 0; i <= (strings.length - 1)/2; i++) {
             String string = strings[i];
